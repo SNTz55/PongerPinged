7b38b9aeb8f5fdd57a450c6636e4aeb2cfc11ad9
SNTz55 <101784360+SNTz55@users.noreply.github.com>
2025-11-13 01:14:18 +0530

    Update PongGame.java

--- PongGame.java
+++ PongGame.java
@@ -1,6 +1,7 @@
 import java.awt.*;
 import java.awt.event.*;
 import javax.swing.*;
+import java.util.*;
 
 class GamePanel extends JPanel implements Runnable{
 
@@ -155,5 +156,6 @@ class GamePanel extends JPanel implements Runnable{
         }
     }
 }
+
 
 
