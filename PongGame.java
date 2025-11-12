ca8fc9377764f1e8f89542e05407febf1474a043
SNTz55 <101784360+SNTz55@users.noreply.github.com>
2025-11-13 01:28:08 +0530

    Update PongGame.java

--- PongGame.java
+++ PongGame.java
@@ -155,3 +155,137 @@ class GamePanel extends JPanel implements Runnable{
         }
     }
 }
+//**********************************************************
+
+class Paddle extends Rectangle{
+    int id;
+    int yVelocity;
+    int speed = 11;
+
+    Paddle(int x, int y, int PADDLE_WIDTH, int PADDLE_HEIGHT, int id){
+        super(x,y,PADDLE_WIDTH,PADDLE_HEIGHT);
+        this.id=id;
+    }
+
+    public void keyPressed(KeyEvent e) {
+        switch(id) {
+            case 1:
+                if(e.getKeyCode()==KeyEvent.VK_W) {
+                    setYDirection(-speed);
+                }
+                if(e.getKeyCode()==KeyEvent.VK_S) {
+                    setYDirection(speed);
+                }
+                break;
+            case 2:
+                if(e.getKeyCode()==KeyEvent.VK_UP) {
+                    setYDirection(-speed);
+                }
+                if(e.getKeyCode()==KeyEvent.VK_DOWN) {
+                    setYDirection(speed);
+                }
+                break;
+        }
+    }
+
+    public void keyReleased(KeyEvent e) {
+        switch(id) {
+            case 1:
+                if(e.getKeyCode()==KeyEvent.VK_W) {
+                    setYDirection(0);
+                }
+                if(e.getKeyCode()==KeyEvent.VK_S) {
+                    setYDirection(0);
+                }
+                break;
+            case 2:
+                if(e.getKeyCode()==KeyEvent.VK_UP) {
+                    setYDirection(0);
+                }
+                if(e.getKeyCode()==KeyEvent.VK_DOWN) {
+                    setYDirection(0);
+                }
+                break;
+        }
+    }
+
+    public void setYDirection(int yDirection) {
+        yVelocity = yDirection;
+    }
+
+    public void move() {
+        y= y + yVelocity;
+    }
+
+    public void draw(Graphics g) {
+        if(id==1)
+            g.setColor(Color.blue);
+        else
+            g.setColor(Color.red);
+        g.fillRect(x, y, width, height);
+    }
+}
+//**********************************************************
+
+class Ball extends Rectangle{
+    Random random;
+    int xVelocity;
+    int yVelocity;
+    int initialSpeed = 2;
+
+    Ball(int x, int y, int width, int height){
+        super(x,y,width,height);
+        random = new Random();
+        int randomXDirection = random.nextInt(2);
+        if(randomXDirection == 0)
+            randomXDirection--;
+        setXDirection(randomXDirection*initialSpeed);
+
+        int randomYDirection = random.nextInt(2);
+        if(randomYDirection == 0)
+            randomYDirection--;
+        setYDirection(randomYDirection*initialSpeed);
+
+    }
+
+    public void setXDirection(int randomXDirection) {
+        xVelocity = randomXDirection;
+    }
+
+    public void setYDirection(int randomYDirection) {
+        yVelocity = randomYDirection;
+    }
+
+    public void move() {
+        x += xVelocity;
+        y += yVelocity;
+    }
+
+    public void draw(Graphics g) {
+        g.setColor(Color.white);
+        g.fillOval(x, y, height, width);
+    }
+}
+//**********************************************************
+
+class Score extends Rectangle{
+    static int GAME_WIDTH;
+    static int GAME_HEIGHT;
+    int player1;
+    int player2;
+
+    Score(int GAME_WIDTH, int GAME_HEIGHT){
+        Score.GAME_WIDTH = GAME_WIDTH;
+        Score.GAME_HEIGHT = GAME_HEIGHT;
+    }
+
+    public void draw(Graphics g) {
+        g.setColor(Color.white);
+        g.setFont(new Font("Consolas",Font.PLAIN,60));
+
+        g.drawLine(GAME_WIDTH/2, 0, GAME_WIDTH/2, GAME_HEIGHT);
+
+        g.drawString(String.valueOf(player1/10)+String.valueOf(player1%10), (GAME_WIDTH/2)-85, 50);
+        g.drawString(String.valueOf(player2/10)+String.valueOf(player2%10), (GAME_WIDTH/2)+20, 50);
+    }
+}
